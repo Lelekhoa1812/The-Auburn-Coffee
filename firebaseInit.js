a9e7ed6094bf5e2215cb1f23f5320d8b082e71ca
Khoa Le <120232418+Lelekhoa1812@users.noreply.github.com>
2024-09-10 10:17:09 +1000

    Add files via upload

---
+++ firebaseInit.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDM2V0ydr3kFL3UP1vqjIGl3o8mMS2mjYw",
+  authDomain: "the-auburn-coffee.firebaseapp.com",
+  projectId: "the-auburn-coffee",
+  storageBucket: "the-auburn-coffee.appspot.com",
+  messagingSenderId: "522423593553",
+  appId: "1:522423593553:web:20b9cfbd18df09e22a6670",
+  measurementId: "G-1TY2HBT6W7"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
