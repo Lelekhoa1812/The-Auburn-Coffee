3eb72061fc8c7641eb5c06ddfbdeba9b490e5c80
Khoa Le <120232418+Lelekhoa1812@users.noreply.github.com>
2024-09-10 12:03:15 +1000

    Update firebaseInit.js

--- firebaseInit.js
+++ firebaseInit.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDM2V0ydr3kFL3UP1vqjIGl3o8mMS2mjYw",
-  authDomain: "the-auburn-coffee.firebaseapp.com",
-  projectId: "the-auburn-coffee",
-  storageBucket: "the-auburn-coffee.appspot.com",
-  messagingSenderId: "522423593553",
-  appId: "1:522423593553:web:20b9cfbd18df09e22a6670",
-  measurementId: "G-1TY2HBT6W7"
+    apiKey: "AIzaSyDM2V0ydr3kFL3UP1vqjIGl3o8mMS2mjYw",
+    authDomain: "the-auburn-coffee.firebaseapp.com",
+    projectId: "the-auburn-coffee",
+    storageBucket: "the-auburn-coffee.appspot.com",
+    messagingSenderId: "522423593553",
+    appId: "1:522423593553:web:20b9cfbd18df09e22a6670",
+    measurementId: "G-1TY2HBT6W7"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
